970a3b7cc0679fea4ed90e994739b60f27d0eb2c
Estevao <estevao.vieira@lynxprocess.com>
2019-11-12 18:00:38 -0300

    a

--- UNA_Fighters/Assets/Scenes/MenuScript.cs
+++ UNA_Fighters/Assets/Scenes/MenuScript.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
@@ -13,8 +14,8 @@ public class MenuScript : MonoBehaviour
     public Image exitButton;
     public RectTransform exitRect;
 
-    private Vector2 selectedSize = new Vector2(500, 500);
-    private Vector2 normalSize = new Vector2(385, 385);
+    private Vector2 selectedSize = new Vector2(700, 700);
+    private Vector2 normalSize = new Vector2(400, 400);
 
     private void Start()
     {
@@ -45,13 +46,23 @@ public class MenuScript : MonoBehaviour
                 }
         }
 
-        if (Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.S) || Input.GetKeyDown(KeyCode.Joystick1Button0) || Input.GetKey(KeyCode.Joystick1Button13)
-            || Input.GetKeyDown(KeyCode.Joystick2Button0) || Input.GetKey(KeyCode.Joystick2Button13))
+        if (Input.anyKeyDown)
+        {
+            foreach(KeyCode kcode in Enum.GetValues(typeof(KeyCode)))
+{
+                if (Input.GetKey(kcode))
+                    Debug.Log("KeyCode down: " + kcode);
+            }
+        }
+        
+
+        if (Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.S) || (Input.GetAxis("Vertical") == -1) || (Input.GetAxis("Vertical") == 1f)
+            || (Input.GetAxis("p2x") == 1f) || (Input.GetAxis("p2y") == -1f))
         {
             selectedOption = selectedOption == Options.Start ? Options.Exit : Options.Start;
         }
 
-        if (Input.GetKeyDown(KeyCode.Return))
+        if (Input.GetKeyDown(KeyCode.Return) || Input.GetKeyDown(KeyCode.Joystick1Button9) || Input.GetKey(KeyCode.Joystick2Button9))
         {
             if(selectedOption == Options.Start)
             {
